d855308d8635e1b3d71cb06ba086adbb12cc4f0e
ChampPhil <109100465+ChampPhil@users.noreply.github.com>
2024-03-31 00:37:54 -0400

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -116,25 +116,53 @@ def test_step(model: torch.nn.Module,
 class FashionMNISTV1(nn.Module):
   def __init__(self, hidden_units, in_features, out_features):
     super().__init__()
-    self.stack = nn.Sequential(
-        nn.Flatten(),
-        nn.Linear(in_features=in_features, out_features=hidden_units),
+    self.block_1 = nn.Sequential(
+        nn.Conv2d(in_channels=input_shape, 
+                  out_channels=hidden_units, 
+                  kernel_size=3, # how big is the square that's going over the image?
+                  stride=1, # default
+                  padding=1),# options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number 
         nn.ReLU(),
-        nn.Linear(in_features=hidden_units, out_features=hidden_units),
+        nn.Conv2d(in_channels=hidden_units, 
+                  out_channels=hidden_units,
+                  kernel_size=3,
+                  stride=1,
+                  padding=1),
         nn.ReLU(),
-        nn.Linear(in_features=hidden_units, out_features=out_features)
-
+        nn.MaxPool2d(kernel_size=2,
+                     stride=2) # default stride value is same as kernel_size
     )
+    self.block_2 = nn.Sequential(
+        nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2)
+    )
+    self.classifier = nn.Sequential(
+        nn.Flatten(),
+        # Where did this in_features shape come from? 
+        # It's because each layer of our network compresses and changes the shape of our inputs data.
+        nn.Linear(in_features=hidden_units*7*7, 
+                  out_features=output_shape)
+    )
+    
+  def forward(self, x: torch.Tensor):
+    x = self.block_1(x)
+    # print(x.shape)
+    x = self.block_2(x)
+    # print(x.shape)
+    x = self.classifier(x)
+    # print(x.shape)
+    return x
 
-  def forward(self, x: torch.Tensor) -> torch.Tensor:
-    return self.stack(x)
 
 torch.manual_seed(42)
 model1 = FashionMNISTV1(
-    hidden_units=64,
-    in_features=28*28,
-    out_features=len(class_names)
-).to(device)
+   input_shape=1, 
+    hidden_units=10, 
+    output_shape=len(class_names)).to(device)
+
 
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(params=model1.parameters(), lr=0.1)
